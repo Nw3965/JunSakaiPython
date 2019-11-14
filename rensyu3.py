e0be9b970fa03d5414e3fc0aff2b36099e9b75fa
Nw3965 <hwrd231@gmail.com>
2019-11-14 12:45:27 +0900

    Create rensyu3.py

---
+++ rensyu3.py
@@ -0,0 +1,6 @@
+for i in range (1,1000):
+    if i%2 == 0:
+        print(i,"This is Even") #偶数
+    else:
+        print(i,"This is odd number" )#奇数
+#Use comma , to print number and string same time
